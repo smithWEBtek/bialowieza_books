37b0ecdb2d7cdb7762a6af857ce6b121e0dd9bd7
github username <github email address>
2018-08-21 10:00:33 -0500

    added resource generator authors

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,10 @@
+class AuthorsController < ApplicationController
+
+  def index
+    @authors = Author.all
+  end
+
+  def show
+    @author = Author.find_by(id: params[:id])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_144957) do
+ActiveRecord::Schema.define(version: 2018_08_21_145806) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
