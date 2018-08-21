d299e1a356cf8503f4d65e757128e40be49c0ebe
github username <github email address>
2018-08-21 10:24:24 -0500

    added resource generator for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_151144) do
+ActiveRecord::Schema.define(version: 2018_08_21_152242) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 2018_08_21_151144) do
   create_table "order_items", force: :cascade do |t|
     t.integer "book_id"
     t.integer "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create users
+5.times do
+  @user = User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length = 6, max_length = 12),
+  )
+  @cart = Cart.create(user_id: @user.id)
+end
+
+# Create Authors
+10.times do
+  @author = Author.create(
+    name: Faker::Book.author
+  )
+end
+@authors = Author.all.size
+
+# Create Books
+20.times do
+  @book = Book.create(
+    title: Faker::Book.title,
+    genre: Faker::Book.genre,
+    price: rand(5..30),
+    summary: Faker::Hipster.paragraphs(6),
+    author_id: rand(1..@authors)
+  )
+end
+
+@books = Book.all
+@users = User.all.size
+
+# Create List_Items
+@books.each do |book|
+  ListItem.create(
+    shopping_cart_id: rand(1..@users),
+    book_id: book.id
+  )
+end
