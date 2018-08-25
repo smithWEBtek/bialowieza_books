b3677928fd6e3d3877b39eb38a81809ce4cd8e5e
github username <github email address>
2018-08-25 09:55:19 -0500

    added book.make_title_case method and before_save callback

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,10 @@ class Book < ApplicationRecord
   scope :most_expensive, -> {order(price: :desc).limit(3)}
   scope :cheapest, -> {order(price: :asc).limit(3)}
   scope :alphabetical, -> {order(title: :asc).limit(10)}
+  before_save :make_title_case
+
+  def make_title_case
+    self.genre = self.genre.titlecase 
+    self.title = self.title.titlecase
+  end
 end
